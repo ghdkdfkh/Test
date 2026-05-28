9fd8e949d2c27ce25b5cfc5c87a433344e58734f
daas <filip.mso83@mail.ru>
2026-05-28 11:58:25 +0300

    Добавил ввод пароля и проверку его правильности

--- Test project/Test project/Program.cs
+++ Test project/Test project/Program.cs
@@ -10,13 +10,52 @@ namespace Test_project
     {
         static void Main(string[] args)
         {
-            string input;
+            UserAuthorise();
 
-            Console.WriteLine("Введите ваше имя");
-            input = Console.ReadLine();
-
-            Console.WriteLine($"Ваше имя {input}");
+            Console.WriteLine("Press any key...");
             Console.ReadKey();
+        }
+
+        private static void UserAuthorise()
+        {
+            const string Password = "hu";
+            int tryiescount = 3;
+            string passwordInput = null;
+            string userName;
+
+            userName = EnterUserName();
+
+            PasswordInput(Password, ref tryiescount, ref passwordInput, userName);
+        }
+
+        private static void PasswordInput(string Password, ref int tryiescount, ref string passwordInput, string userName)
+        {
+            while (passwordInput != Password && tryiescount > 0)
+            {
+                tryiescount--;
+                Console.WriteLine("Введите пароль:");
+                passwordInput = Console.ReadLine();
+
+                ShowAuthMessage(Password, passwordInput, userName, tryiescount);
+            }
+        }
+
+        private static string EnterUserName()
+        {
+            string input;
+            Console.WriteLine("Введите ваше имя");
+            input = Console.ReadLine(); 
+
+            Console.WriteLine($"\nВаше имя {input}");
+            return input;
+        }
+
+        private static void ShowAuthMessage(string Password, string passwordInput, string input, object tryiescount)
+        {
+            if (passwordInput == Password)
+                Console.WriteLine("Password.OK");
+            else
+                Console.WriteLine($">>> {input}, Password.IS_NOT_OK !!!! осталось {tryiescount} попыток <<<");
         }
     }
 }
