ffcd45f72ea50ea46bbb9af9c12f2538b021e0bc
daas <filip.mso83@mail.ru>
2026-05-30 16:03:30 +0300

    Решили задачу простым способом

---
+++ fdkjsdfkjhds/ConsoleAppSumNumbers/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppSumNumbers
+{
+    //2.4) ДЗ: Сумма чисел
+    //С помощью Random получить число number, которое не больше 100. 
+    //Найти сумму всех положительных чисел меньше number(включая число), которые кратные 3 или 5.
+    //(К примеру, это числа 3, 5, 6, 9, 10, 12, 15 и т.д.)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tgt_1 = 3;
+            int tgt_2 = 5;
+
+            int sumNumbersResult_1;
+            int sumNumbersResult_2 = 0;
+
+
+            Random rand = new Random();
+            int randomNumber = rand.Next(1, 25);
+            Console.WriteLine($"randomNumber = {randomNumber}\n");
+
+            sumNumbersResult_1 = SumMultiplesNums(tgt_1, randomNumber, ConsoleColor.Yellow);
+            Console.WriteLine();
+            sumNumbersResult_2 = SumMultiplesNums(tgt_2, randomNumber, ConsoleColor.Cyan);
+
+            Console.WriteLine($"\nСумма чисел кратных {tgt_1} = {sumNumbersResult_1}");
+            Console.WriteLine($"Сумма чисел кратных {tgt_2} = {sumNumbersResult_2}");
+
+            //for (int i = 0; i < randomNumber; i++)
+            //{
+            //    Console.WriteLine($"i = {i}");
+            //    var currentNum = i + 1;
+            //    Console.WriteLine($"currentNum = {currentNum}");
+            //    Console.WriteLine();
+
+            //    if (true)
+            //    {
+
+            //    }
+            //}
+            //while (randomNumber>0)
+            //{
+            //    if (true)
+            //    {
+
+            //    }
+
+            //}
+            Console.ReadKey();
+        }
+
+        private static int SumMultiplesNums(int tgt, int randomNumber, ConsoleColor color)
+        {
+            int sumNumbers = 0;
+            var iteration = randomNumber / tgt;
+            int sumNumbersResult = 0;
+
+            for (int i = 0; i < iteration; i++)
+            {
+                sumNumbers = sumNumbers + tgt;
+                sumNumbersResult += sumNumbers;
+                var n = i + 1;
+                Console.ForegroundColor = color;
+                Console.WriteLine($"число №{n} - " + sumNumbers);
+                Console.ResetColor();
+            }
+
+            return sumNumbersResult;
+        }
+    }
+}
---
+++ fdkjsdfkjhds/fdkjsdfkjhds/Program.cs
@@ -0,0 +1,204 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fdkjsdfkjhds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float rubInwallet;
+            float dollarsInwallet;
+            float euroInwallet;
+            int rubToUsd = 60, rubToEuro = 70, usdToRub = 62, usdToEuro = 78, euroToUsd = 80, euroToRub = 72;
+            string desiredOperation;
+            float exchanceCurrencyCount;
+
+            ShowText("Добро пожаловать в обменник");
+            ShowText("Ваш баланс рублей");
+            rubInwallet = Convert.ToSingle(Console.ReadLine());
+            ShowText("Ваш баланс долларов");
+            dollarsInwallet = Convert.ToSingle(Console.ReadLine());
+            ShowText("Ваш баланс евро");
+            euroInwallet = Convert.ToSingle(Console.ReadLine());
+
+            ShowText("Выберите операцию");
+            ShowText("1 - рубли в доллары");
+            ShowText("2 - рубли в евро");
+            ShowText("3 - доллары в рубли");
+            ShowText("4 - доллары в евро");
+            ShowText("5 - евро в рубли");
+            ShowText("6 - евро в доллары");
+            ShowText("Ваш выбор ");
+
+            desiredOperation = Console.ReadLine();
+            switch (desiredOperation)
+            {
+                case "1":
+                    exchanceCurrencyCount = RubToUsd(ref rubInwallet, ref dollarsInwallet, rubToUsd);
+                    break;
+                case "2":
+                    exchanceCurrencyCount = RubToEu(ref rubInwallet, ref euroInwallet, rubToEuro);
+                    break;
+                case "3":
+                    exchanceCurrencyCount = UsdToRub(ref rubInwallet, ref dollarsInwallet, usdToRub);
+                    break;
+                case "4":
+                    exchanceCurrencyCount = UsdtoEu(ref dollarsInwallet, ref euroInwallet, usdToEuro);
+                    break;
+                case "5":
+                    exchanceCurrencyCount = EuToRub(ref rubInwallet, ref euroInwallet, euroToRub);
+                    break;
+                case "6":
+                    exchanceCurrencyCount = EuToUsd(ref dollarsInwallet, ref euroInwallet, euroToUsd);
+                    break;
+                default:
+                    Console.WriteLine("Выбрана неверная операция");
+                    break;
+            }
+            Console.WriteLine($"Ваш баланс рублей:{rubInwallet}, евро: {euroInwallet}, долларов: {dollarsInwallet}");
+
+
+
+
+
+
+            #region dasd
+
+            //bool Isworking = true;
+            //string input;
+
+            //while (Isworking)
+            //{
+            //    Console.WriteLine("Программа работает");
+            //    input = Console.ReadLine();
+            //    if (input == "exit")
+            //    {
+            //        Isworking = false;
+            //    }
+            //}
+            #endregion
+        }
+        static void ShowText(string text)
+        {
+            Console.WriteLine();
+        }
+
+        private static float UsdtoEu(ref float dollarsInwallet, ref float euroInwallet, int usdToEuro)
+        {
+            float exchanceCurrencyCount;
+            Console.WriteLine("Обмен долларов на евро");
+            Console.Write("Сколько вы хотите обменять?");
+            exchanceCurrencyCount = Convert.ToSingle(Console.ReadLine());
+            if (dollarsInwallet >= exchanceCurrencyCount)
+            {
+                dollarsInwallet -= exchanceCurrencyCount;
+                euroInwallet += exchanceCurrencyCount / usdToEuro;
+            }
+            else
+            {
+                Console.WriteLine("Недопустимое значение");
+            }
+
+            return exchanceCurrencyCount;
+        }
+
+        private static float EuToRub(ref float rubInwallet, ref float euroInwallet, int euroToRub)
+        {
+            float exchanceCurrencyCount;
+            Console.WriteLine("Обмен евро на рубли");
+            Console.Write("Сколько вы хотите обменять?");
+            exchanceCurrencyCount = Convert.ToSingle(Console.ReadLine());
+            if (euroInwallet >= exchanceCurrencyCount)
+            {
+                euroInwallet -= exchanceCurrencyCount;
+                rubInwallet += exchanceCurrencyCount * euroToRub;
+            }
+            else
+            {
+                Console.WriteLine("Недопустимое значение");
+            }
+
+            return exchanceCurrencyCount;
+        }
+
+        private static float EuToUsd(ref float dollarsInwallet, ref float euroInwallet, int euroToUsd)
+        {
+            float exchanceCurrencyCount;
+            Console.WriteLine("Обмен евро на доллары");
+            Console.Write("Сколько вы хотите обменять?");
+            exchanceCurrencyCount = Convert.ToSingle(Console.ReadLine());
+            if (euroInwallet >= exchanceCurrencyCount)
+            {
+                euroInwallet -= exchanceCurrencyCount;
+                dollarsInwallet += exchanceCurrencyCount / euroToUsd;
+            }
+            else
+            {
+                Console.WriteLine("Недопустимое значение");
+            }
+
+            return exchanceCurrencyCount;
+        }
+
+        private static float UsdToRub(ref float rubInwallet, ref float dollarsInwallet, int usdToRub)
+        {
+            float exchanceCurrencyCount;
+            Console.WriteLine("Обмен долларов на рубли");
+            Console.Write("Сколько вы хотите обменять?");
+            exchanceCurrencyCount = Convert.ToSingle(Console.ReadLine());
+            if (dollarsInwallet >= exchanceCurrencyCount)
+            {
+                dollarsInwallet -= exchanceCurrencyCount;
+                rubInwallet += exchanceCurrencyCount * usdToRub;
+            }
+            else
+            {
+                Console.WriteLine("Недопустимое значение");
+            }
+
+            return exchanceCurrencyCount;
+        }
+
+        private static float RubToEu(ref float rubInwallet, ref float euroInwallet, int rubToEuro)
+        {
+            float exchanceCurrencyCount;
+            Console.WriteLine("Обмен рублей на евро");
+            Console.Write("Сколько вы хотите обменять?");
+            exchanceCurrencyCount = Convert.ToSingle(Console.ReadLine());
+            if (rubInwallet >= exchanceCurrencyCount)
+            {
+                rubInwallet -= exchanceCurrencyCount;
+                euroInwallet += exchanceCurrencyCount / rubToEuro;
+            }
+            else
+            {
+                Console.WriteLine("Недопустимое значение");
+            }
+
+            return exchanceCurrencyCount;
+        }
+
+        private static float RubToUsd(ref float rubInwallet, ref float dollarsInwallet, int rubToUsd)
+        {
+            float exchanceCurrencyCount;
+            Console.WriteLine("Обмен рублей на доллары");
+            Console.Write("Сколько вы хотите обменять?");
+            exchanceCurrencyCount = Convert.ToSingle(Console.ReadLine());
+            if (rubInwallet >= exchanceCurrencyCount)
+            {
+                rubInwallet -= exchanceCurrencyCount;
+                dollarsInwallet += exchanceCurrencyCount / rubToUsd;
+            }
+            else
+            {
+                Console.WriteLine("Недопустимое значение");
+            }
+
+            return exchanceCurrencyCount;
+        }
+    }
+}
